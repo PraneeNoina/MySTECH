37b062dc060af34ea5920b2962af7004cc429fee
pranee <noinapranee@gmail.com>
2017-04-04 15:59:05 +0700

    initialView

--- app/src/main/java/com/example/pranee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pranee/myapplication/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.pranee.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial View  การผูกตัวแปรกับ View ที่อยู่ที่ Activity
+        initialView();
+
+    }   // Main Method นี่คือ Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+
+}   //Main Class นี่คือ Class หลัก
